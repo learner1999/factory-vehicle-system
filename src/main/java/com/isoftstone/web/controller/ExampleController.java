ab55be690b0e2936aebdf99c992aca1789937ea1
learner <roadoflearning@live.com>
2016-05-12 19:24:10 +0800

    重新编写 restful 示例

---
+++ src/main/java/com/isoftstone/web/controller/ExampleController.java
@@ -0,0 +1,158 @@
+package com.isoftstone.web.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.isoftstone.web.dao.TestUserDao2;
+import com.isoftstone.web.pojo.TestUser2;
+
+@RestController
+public class ExampleController {
+	
+	private TestUserDao2 userDao = new TestUserDao2();
+	
+	
+	/***
+	 * 查询所有用户
+	 * url:http://www.example.com/api/user/
+	 * method:GET
+	 * @return 所有用户的信息
+	 */
+	@RequestMapping(value = "/api/user", method = RequestMethod.GET)
+	public ResponseEntity<List<TestUser2>> listAllUsers() {
+		List<TestUser2> users = userDao.findAllUsers();
+		
+		// 如果查询不到用户，将 http 状态码设置为 NO_CONTENT
+		if(users.isEmpty()) {
+			return new ResponseEntity<List<TestUser2>>(HttpStatus.NO_CONTENT);
+		}
+		
+		// 如果正确查询，则返回所有用户，同时将 http 状态码设为 OK
+		return new ResponseEntity<List<TestUser2>>(users, HttpStatus.OK);
+	}
+	
+	
+	
+	/***
+	 * 获取单个用户信息，指定 id
+	 * url:http://www.example.com/api/user/1
+	 * method:GET
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/api/user/{id}", method = RequestMethod.GET)  // {id} 表示路径中这个位置是一个变化的参数，参数名为 id
+	public ResponseEntity<TestUser2> getUser(@PathVariable("id") int id) {  // 在参数 id 前添加 PathVariable，表示这个参数对应路径中的 id 值
+		System.out.println("获取用户通过id=" + id);
+		
+		TestUser2 user = userDao.findById(id);
+		if(null == user) {
+			System.out.println("用户没有找到 id=" + id);
+			return new ResponseEntity<TestUser2>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<TestUser2>(user, HttpStatus.OK);
+	}
+	
+	
+	/***
+	 * 创建一个用户
+	 * url：http://www.example.com/api/user
+	 * method：POST
+	 * @param user 全哥提交 json 格式的完整的 user 信息
+	 * @return 创建成功，返回创建的用户的信息
+	 */
+	@RequestMapping(value = "/api/user", method = RequestMethod.POST)
+	// 下面一行 RequestBody 这个注解表示全哥会传一个 json对象过来，对应 TestUser2，框架会自动将信息提取到参数 user 中，测试方法在 coding
+	public ResponseEntity<TestUser2> createUser(@RequestBody TestUser2 user) { 
+		System.out.println("创建用户 " + user.getUsername());
+		
+		// 检查提交的数据是否完整，如果不完整，将状态码设置为 BAD_REQUEST
+		if(null == user.getUsername() || null == user.getPassword() || 0 == user.getAuthority()) {
+			return new ResponseEntity<TestUser2>(HttpStatus.BAD_REQUEST);
+		}
+		
+		// 检测用户名是否冲突
+		if(userDao.isUserExist(user)) {
+			System.out.println("用户 " + user.getUsername() + "已存在");
+			return new ResponseEntity<TestUser2>(HttpStatus.CONFLICT);
+		}
+		
+		
+		// 创建用户
+		if(userDao.createUser(user)) {
+			return new ResponseEntity<TestUser2>(user, HttpStatus.CREATED);
+		}
+		
+		// 如果出现异常，将状态码设为 internal server error(寻找更好的解决方案中……)
+		return new ResponseEntity<TestUser2>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	/***
+	 * 修改对应 id 用户的信息，可以只修改部分……我们不用 PATCH 了
+	 * @param id
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping(value = "/api/user/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<TestUser2> updateUser(@PathVariable("id") int id, @RequestBody TestUser2 user) {
+		System.out.println("更新用户信息，id=" + id);
+		
+		// 通过 id 找到用户当前的信息
+		TestUser2 currentUser = userDao.findById(id);
+		
+		if(null == currentUser) {
+			System.out.println("没有找到该用户，id=" + id);
+			return new ResponseEntity<TestUser2>(HttpStatus.NOT_FOUND);
+		}
+		
+		// 修改需要修改的属性
+		if(user.getUsername() != null)
+			currentUser.setUsername(user.getUsername());
+		if(user.getPassword() != null) 
+			currentUser.setPassword(user.getPassword());
+		if(user.getAuthority() != 0)
+			currentUser.setAuthority(user.getAuthority());
+		
+		// 数据库操作
+		if(userDao.updateUser(id, currentUser)) {
+			return new ResponseEntity<TestUser2>(currentUser, HttpStatus.OK);
+		}
+		
+		// 如果出现异常，将状态码设为 internal server error(寻找更好的解决方案中……)
+		return new ResponseEntity<TestUser2>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	/***
+	 * 删除对应 id 用户信息
+	 * @param id 用户id
+	 * @return
+	 */
+	@RequestMapping(value = "/api/user/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<TestUser2> deleteUser(@PathVariable("id") int id) {
+		System.out.println("删除一个用户 id=" + id);
+		
+		// 检测对应 id 用户是否存在，不存在将状态码设为 NOT_FOUND
+		TestUser2 user = userDao.findById(id);
+		if(null == user) {
+			System.out.println("找不到 id=" + id + " 的用户");
+			return new ResponseEntity<TestUser2>(HttpStatus.NOT_FOUND);
+		}
+		
+		// 数据库操作
+		if(userDao.deleteUser(id)) {
+			System.out.println();
+			return new ResponseEntity<TestUser2>(HttpStatus.NO_CONTENT);
+		}
+		
+		return  new ResponseEntity<TestUser2>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
---
+++ src/main/java/com/isoftstone/web/dao/TestUserDao2.java
@@ -0,0 +1,241 @@
+package com.isoftstone.web.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.isoftstone.web.pojo.TestUser2;
+import com.isoftstone.web.util.JdbcUtil;
+
+public class TestUserDao2 {
+
+	/***
+	 * 查找所有用户
+	 * 
+	 * @return 所有用户构成的 list
+	 */
+	public List<TestUser2> findAllUsers() {
+		
+		// 创建数据库连接时需要的对象
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+
+		// 创建一个 List 用于存放 User 对象
+		List<TestUser2> userList = new ArrayList<>();
+
+		// 数据库查询语句
+		String strSql = "SELECT * FROM `user`";
+
+		try {
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(strSql);  // 这个地方用了 prepareStatement，主要目的是防止 sql 注入
+			result = stmt.executeQuery();
+			while (result.next()) {
+				TestUser2 user = fetchData(result);  // 写了一个 fetchData 函数便于获取数据库返回的数据
+				userList.add(user);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(result, stmt, conn);  // 在最后记得关闭数据库连接
+		}
+
+		return userList;
+	}
+
+	/***
+	 * 通过用户 id 查找用户信息
+	 * 
+	 * @param id
+	 *            用户id
+	 * @return 用户信息
+	 */
+	public TestUser2 findById(int id) {
+		
+		// jdbc 必备
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+
+		// 用来存放查询得到的用户信息
+		TestUser2 user = null;
+		
+		// 数据库查询字符串，其中的 '?' 号是一个占位符，预编译的方式（prepareStatement）需要这种写法
+		String strSql = "SELECT * FROM `user` WHERE id=?";
+
+		try {
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(strSql);
+			stmt.setInt(1, id);  // 将 id 值替换至  '?' 号占位的地方
+			result = stmt.executeQuery();
+			if (result.next()) {
+				user = fetchData(result);  // 提取数据库返回的信息
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(result, stmt, conn);  // 关闭数据库连接
+		}
+
+		return user;
+	}
+
+	/***
+	 * 查询数据库中是否已经存在相同用户名用户
+	 * 
+	 * @param user
+	 *            查询的用户对象
+	 * @return 是否存在
+	 */
+	public boolean isUserExist(TestUser2 user) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+
+		String strSql = "SELECT * FROM `user` WHERE username=?";
+
+		try {
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(strSql);
+			stmt.setString(1, user.getUsername());
+			result = stmt.executeQuery();
+			if (result.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(result, stmt, conn);
+		}
+
+		return false;
+	}
+
+	/***
+	 * 将 ResultSet 中的数据提取到 user 对象中
+	 * 
+	 * @param result
+	 *            执行数据库查询后返回的 ResultSet
+	 * @return user对象
+	 * @throws SQLException
+	 */
+	private TestUser2 fetchData(ResultSet result) throws SQLException {
+		TestUser2 user = new TestUser2();
+
+		user.setId(result.getInt("id"));
+		user.setUsername(result.getString("username"));
+		user.setPassword(result.getString("password"));
+		user.setAuthority(result.getInt("authority"));
+
+		return user;
+	}
+
+	/***
+	 * 向数据库中插入一个用户
+	 * 
+	 * @param user
+	 *            用户信息
+	 * @return 成功返回 true，失败返回 false
+	 */
+	public boolean createUser(TestUser2 user) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		// 3 个占位符，分别对应位置为 1,2,3
+		String strSql = "INSERT INTO `user` (username, password, authority) VALUES (?, ?, ?)";
+
+		try {
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(strSql);
+			
+			// 替换占位符位置的值
+			stmt.setString(1, user.getUsername());
+			stmt.setString(2, user.getPassword());
+			stmt.setInt(3, user.getAuthority());
+			
+			int counter = stmt.executeUpdate();
+			if (1 == counter) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(null, stmt, conn);
+		}
+
+		return false;
+	}
+
+	/***
+	 * 修改数据库中对应 id 用户信息
+	 * 
+	 * @param id
+	 *            用户id
+	 * @param user
+	 *            用户信息
+	 * @return 成功返回 true，失败返回 false
+	 */
+	public boolean updateUser(int id, TestUser2 user) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		String strSql = "UPDATE `user` SET `username`=?, `password`=?, `authority`=? WHERE (`id`=?)";
+
+		try {
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(strSql);
+			stmt.setString(1, user.getUsername());
+			stmt.setString(2, user.getPassword());
+			stmt.setInt(3, user.getAuthority());
+			stmt.setInt(4, id);
+			int counter = stmt.executeUpdate();
+			if (1 == counter) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(null, stmt, conn);
+		}
+
+		return false;
+	}
+
+	/***
+	 * 删除指定 id 用户信息
+	 * 
+	 * @param id
+	 *            用户id
+	 * @return 成功返回 true，失败返回 false
+	 */
+	public boolean deleteUser(int id) {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		String strSql = "DELETE FROM `user` WHERE (`id`=?)";
+
+		try {
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(strSql);
+			stmt.setInt(1, id);
+			int counter = stmt.executeUpdate();
+			if (1 == counter) {
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(null, stmt, conn);
+		}
+
+		return false;
+	}
+
+}
