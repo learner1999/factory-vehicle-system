3294356182644a7d6d95e0080d0b2ead5cc3baa6
learner <roadoflearning@live.com>
2016-08-04 21:11:06 +0800

    更新 路线报表接口

--- src/main/java/com/isoftstone/web/controller/ExcelController.java
+++ src/main/java/com/isoftstone/web/controller/ExcelController.java
@@ -108,4 +108,99 @@ public class ExcelController {
 		excelRes.setCount(count);
 		return new ResponseEntity<ExcelResult>(excelRes, HttpStatus.OK);
 	}
+	
+	
+	
+	
+	/***
+	 * 给定日期获取这一天各路线的人员乘坐信息
+	 * 
+	 * @param day
+	 * 		给定的日期
+	 * @return 这一天各路线的人员乘坐信息
+	 */
+	@RequestMapping(value = "/api/Excel/routeday", method = RequestMethod.GET)
+	public ResponseEntity<ExcelResult> getRouteExcelByDay(@RequestParam(value="day",required=false) String day)
+	{
+		List<Excel> eList=etDao.getRouteCountByDay(day);
+		if(eList.isEmpty())
+		{
+			return new ResponseEntity<ExcelResult>(HttpStatus.NO_CONTENT);
+		}
+		ExcelResult excelRes=new ExcelResult(eList.size());
+		String sname[]=excelRes.getSname();
+		int count[]=excelRes.getCount();
+		for(int i=0;i<eList.size();i++)
+		{
+			Excel e=eList.get(i);
+			sname[i]=e.getStaName();
+			count[i]=e.getsCount();
+		}
+		excelRes.setSname(sname);
+		excelRes.setCount(count);
+		return new ResponseEntity<ExcelResult>(excelRes, HttpStatus.OK);
+	}
+	
+	/***
+	 * 给定日期获取这一天所在一周各路线的人员乘坐信息
+	 * 
+	 * @param day
+	 * 		给定的日期
+	 * @return 这一天所在一周各路线的人员乘坐信息
+	 */
+	@RequestMapping(value = "/api/Excel/routeweek", method = RequestMethod.GET)
+	public ResponseEntity<ExcelResult> getRouteExcelByWeek(@RequestParam(value="day",required=false) String day)
+	{
+		String[] weekDates = eDao.getWeekStartAndEndDate(day);
+		List<Excel> eList=etDao.getRouteCountByDays(weekDates[0],weekDates[1]);
+		if(eList.isEmpty())
+		{
+			return new ResponseEntity<ExcelResult>(HttpStatus.NO_CONTENT);
+		}
+		ExcelResult excelRes=new ExcelResult(eList.size());
+		String sname[]=excelRes.getSname();
+		int count[]=excelRes.getCount();
+		for(int i=0;i<eList.size();i++)
+		{
+			Excel e=eList.get(i);
+			sname[i]=e.getStaName();
+			count[i]=e.getsCount();
+		}
+		excelRes.setSname(sname);
+		excelRes.setCount(count);
+		return new ResponseEntity<ExcelResult>(excelRes, HttpStatus.OK);
+	}
+	
+	/***
+	 * 给定日期获取这一天所在一月各路线的人员乘坐信息
+	 * 
+	 * @param day
+	 * 		给定的日期
+	 * @return 这一天所在一月各路线的人员乘坐信息
+	 */
+	@RequestMapping(value = "/api/Excel/routemonth", method = RequestMethod.GET)
+	public ResponseEntity<ExcelResult> getRouteExcelByMonth(@RequestParam(value="day",required=false) String day)
+	{
+		String[] monthDates = eDao.getMonthStartAndEndDate(day);
+		List<Excel> eList=etDao.getRouteCountByDays(monthDates[0],monthDates[1]);
+		if(eList.isEmpty())
+		{
+			return new ResponseEntity<ExcelResult>(HttpStatus.NO_CONTENT);
+		}
+		ExcelResult excelRes=new ExcelResult(eList.size());
+		String sname[]=excelRes.getSname();
+		int count[]=excelRes.getCount();
+		for(int i=0;i<eList.size();i++)
+		{
+			Excel e=eList.get(i);
+			sname[i]=e.getStaName();
+			count[i]=e.getsCount();
+		}
+		excelRes.setSname(sname);
+		excelRes.setCount(count);
+		return new ResponseEntity<ExcelResult>(excelRes, HttpStatus.OK);
+	}
 }
+
+
+
--- src/main/java/com/isoftstone/web/dao/ExcelToDbDao.java
+++ src/main/java/com/isoftstone/web/dao/ExcelToDbDao.java
@@ -76,5 +76,81 @@ public class ExcelToDbDao {
 		}
 		return eList;
 	}
+	
+	
+	/***
+	 * 给定日期获取这一天各路线的人员乘坐信息
+	 * 
+	 * @param now
+	 * 		给定的日期
+	 * @return 这一天各路线的人员乘坐信息
+	 */
+	public List<Excel> getRouteCountByDay(String now)
+	{
+		List<Excel> eList=new ArrayList<Excel>();
+		String sql="SELECT CRouteId, SUM(CCount) AS CCount FROM Record2016 WHERE CDate=? GROUP BY CRouteId";
+		try{
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, now);
+			result = stmt.executeQuery();
+			while (result.next()) {
+				Excel excel=new Excel();
+				excel.setStaName(routeIdToName(result.getInt("CRouteId")));
+				excel.setsCount(result.getInt("CCount"));
+				eList.add(excel);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(result, stmt, conn);
+		}
+		return eList;
+	}
+	
+	
+	/***
+	 * 给定起始日期和结束日期，获取这一时间段各路线的总人员乘坐信息
+	 * 
+	 * @param first
+	 * 		起始日期
+	 * @param last
+	 * 		结束日期
+	 * @return 这一天各路线的人员乘坐信息
+	 */
+	public List<Excel> getRouteCountByDays(String first,String last)
+	{
+		List<Excel> eList=new ArrayList<Excel>();
+		String sql="SELECT CRouteId, SUM(CCount) AS CCount FROM Record2016 WHERE CDate BETWEEN ? AND ? GROUP BY CRouteId";
+		try{
+			conn = JdbcUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, first);
+			stmt.setString(2, last);
+			result = stmt.executeQuery();
+			while (result.next()) {
+				Excel excel=new Excel();
+				excel.setStaName(routeIdToName(result.getInt("CRouteId")));
+				excel.setsCount(result.getInt("CCount"));
+				eList.add(excel);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(result, stmt, conn);
+		}
+		return eList;
+	}
+	
+	/**
+	 * 根据路线id返回路线名
+	 * @param routeId
+	 * @return
+	 */
+	private String routeIdToName(int routeId) {
+		int id = routeId % 100;
+		
+		return id + "号线";
+	}
 
 }
