db276b83fb928103270a5d35626bc76da0c8f6ab
learner <roadoflearning@live.com>
2016-08-08 15:52:25 +0800

    修改 登录接口逻辑

--- src/main/java/com/isoftstone/web/controller/LoginController.java
+++ src/main/java/com/isoftstone/web/controller/LoginController.java
@@ -12,8 +12,10 @@ import com.isoftstone.web.dao.LoginDao;
 @RestController
 public class LoginController {
 	LoginDao loginDao = new LoginDao();
+
 	/**
 	 * 判断用户登录
+	 * 
 	 * @param username
 	 * @param password
 	 * @param session
@@ -21,43 +23,37 @@ public class LoginController {
 	 */
 	@RequestMapping(value = "/api/login", method = RequestMethod.POST)
 	public int login(@RequestParam(value = "username") String username,
-			@RequestParam(value = "password") String password , HttpSession session) {
-		if(session.getAttribute("userId") != null) {
-			return 3; //已经登录了
-		}
+			@RequestParam(value = "password") String password, HttpSession session) {
+//		if (session.getAttribute("userId") != null) {
+//			return 3; // 已经登录了
+//		}
 		int id = 0;
-		if(loginDao.is_exist(username, password))
-		{
+		if (loginDao.is_exist(username, password)) {
 			id = loginDao.login(username, password);
 			if (0 == id) {
 				System.out.println(" 0代表普通员工");
-			} else if(1==id){
+			} else if (1 == id) {
 				System.out.println("1代表总务部");
-			}
-			else{
+			} else {
 				System.out.println("2代表行政部");
 			}
 			session.setAttribute("userId", id);
 			return id;
-		}
-		else
-		{
-			if(loginDao.is_username(username)){
+		} else {
+			if (loginDao.is_username(username)) {
 				System.out.println("密码出错");
 				return 4;
-			}
-			else{
+			} else {
 				System.out.println("用户名不存在");
 				return 5;
 			}
-			}
-		}	
-		
+		}
 	}
+
+}
 /*
  *
  * 
 		
  * 
  * */
-
